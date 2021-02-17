3c023b56616f8fff9a5a097f6963d1615f7d617d
Ilham Muhammad <ilhamhd98@gmail.com>
2021-02-17 10:35:35 +0700

    Router Core HMVC : fixing bugs strpos() on deprecated extension

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -236,7 +236,9 @@ class MX_Router extends CI_Router
 	public function set_class($class)
 	{
 		$suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
+		// Disable deprecated feature
+		// if (strpos($class, $suffix) === FALSE) 
+		if( $suffix && strpos($class, $suffix) === FALSE)
 		{
 			$class .= $suffix;
 		}
